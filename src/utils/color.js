b2a187eb626d294c2939b30c80573418a04a77da
BoudBot-SSH <neytic2+5@gmail.com>
2026-07-26 00:04:29 +0200

    first commit

---
+++ src/utils/color.js
@@ -0,0 +1,6 @@
+const color = {
+  red: '#ff0000',
+  gray: '#808080',
+  green: '#2bfb48'
+}
+module.exports = { color };
---
+++ src/utils/link.js
@@ -0,0 +1,6 @@
+const link = {
+  logo: '',
+  default: 'https://i.postimg.cc/sgYLnFMM/default-discord-pfp-blue.jpg',
+  score: 'https://imgs.search.brave.com/WuSyUYq-gromIeIkJ95V2yVBS0GW7Pn37Et1F9X3iwQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wMDAv/NTU1LzI4NS9zbWFs/bC9TY29yZWJvYXJk/X01vY2t1cF9KdW5l/XzAwMi5qcGc'
+}
+module.exports = { link };
